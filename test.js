a949f28cda5f074f952f86d1c9a0bfe5fb045df3
daleldalel <daniel@rezner.co.nz>
2014-03-12 12:56:10 +1300

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 console.log('Simply.js demo!');
 
+var count = parseInt(localStorage.getItem('count')) || 0;
+
 simply.on('singleClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Pressed ' + e.button + '!');
+  if (e.button === 'up') {
+    simply.subtitle(++count);
+  } else if (e.button === 'down') {
+    simply.subtitle(--count);
+  }
+  localStorage.setItem('count', count);
 });
+
+simply.text({ title: 'Counter', subtitle: count });
 
 simply.on('longClick', function(e) {
   console.log(util2.format('long clicked $button!', e));
