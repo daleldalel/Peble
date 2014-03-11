0fd04bf2c43c2f2b3dc74cbbc6172297362c4771
daleldalel <daniel@rezner.co.nz>
2014-03-12 12:44:04 +1300

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ console.log('Simply.js demo!');
 simply.on('singleClick', function(e) {
   console.log(util2.format('single clicked $button!', e));
   simply.vibe();
-  simply.scrollable('Pressed ' + e.button + '!');
+  simply.text('Pressed ' + e.button + '!');
 });
 
 simply.on('longClick', function(e) {
