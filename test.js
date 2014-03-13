924ecc05b9d5897a267fb9a5cfb73144f0b6605d
daleldalel <daniel@rezner.co.nz>
2014-03-13 14:45:36 +1300

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,71 @@
 console.log('Simply.js demo!');
 
-var count = parseInt(localStorage.getItem('count')) || 0;
+var count1 = parseInt(localStorage.getItem('count1')) || 0;
+var swich[]=['lounge','kids','bedroom','micro','hifi','backdoor'];
+var state[6];
+var state[3]=parseInt(localStorage.getItem('micro') || 0;
+var state[4]=parseInt(localStorage.getItem('hifi') || 0;
+var state[5]=parseInt(localStorage.getItem('backdoor') || 0;
 
 simply.on('singleClick', function(e) {
   if (e.button === 'up') {
-    ajax({ url: 'http://rezner.homeftp.net:8202/?G0' }, function(data){
-    simply.subtitle(++count);
-    simply.subtitle('pozar');
+    //ajax({ url: 'http://rezner.homeftp.net:8202/?G0' }, function(data){
+    simply.subtitle(++count1);
+    //simply.subtitle('pozar');
+    if (count>5) {
+      count=0;
+    }
     });
   } else if (e.button === 'down') {
-    simply.subtitle(--count);
+    simply.subtitle(--count1);
+    if (count <0){
+      count=5;
+    }
   }
-  localStorage.setItem('count', count);
+  else if (e.button === 'select') {
+    switch (count){
+      case 0:
+        ajax({ url: 'http://rezner.homeftp.net:8202/?L44' }, function(data);
+        break;
+      case 1:
+        ajax({ url: 'http://rezner.homeftp.net:8202/?L00' }, function(data);
+        break;
+      case 2:
+        ajax({ url: 'http://rezner.homeftp.net:8202/?L14' }, function(data);
+        break;
+      case 3:
+        if (state[3]) {
+          ajax({ url: 'http://rezner.homeftp.net:8202/?S01' }, function(data);
+        }
+        else {ajax({ url: 'http://rezner.homeftp.net:8202/?S11' }, function(data);}
+        state[3]=! state[3];
+        localStorage.setItem('micro', state[3]);
+        break;
+      case 4:
+        if (state[4]) {
+          ajax({ url: 'http://rezner.homeftp.net:8202/?S21' }, function(data);
+        }
+        else {ajax({ url: 'http://rezner.homeftp.net:8202/?S31' }, function(data);}
+        state[4]=! state[4];
+        localStorage.setItem('hifi', state[4]);
+        break;
+      case 5:
+        if (state[5]) {
+          ajax({ url: 'http://rezner.homeftp.net:8202/?S80' }, function(data);
+        }
+        else {ajax({ url: 'http://rezner.homeftp.net:8202/?S90' }, function(data);}
+        state[5]=! state[5];
+        localStorage.setItem('backdoor', state[5]);
+        break;
+    }
+  }
+  localStorage.setItem('count', count1);
+  
 });
 
-simply.text({ title: 'Counter', subtitle: count });
+simply.text({ title: 'Control', subtitle: swich[count1] });
 
-simply.on('longClick', function(e) {
+/*simply.on('longClick', function(e) {
   console.log(util2.format('long clicked $button!', e));
   simply.vibe();
   simply.scrollable(e.button !== 'select');
@@ -28,6 +77,6 @@ simply.on('accelTap', function(e) {
 });
 
 simply.setText({
-  title: 'Test Demo!',
-  body: 'Moj test!',
-}, true);
+  title: 'control:',
+  //body: 'Moj test!',
+}, true);*/
